18c9a37364651820e45c573c4ea4ff82ff10bb09
marcona <arnaud.marconnet@gmail.com>
2020-11-11 07:47:51 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <div>
+    <h1>
+      {{name}}</br>
+      {{reponse}}
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+
+  export default Vue.extend({
+    data: function() {
+      return {
+        name: 'Hello Eliott, ca va ?!',
+        reponse: 'Oui perros',
+      }
+    },
+  });
+</script>
+
+<style lang="scss">
+$primary-color: white;
+$bg: black;
+h1 {
+  color: $primary-color;
+  background-color: $bg;
+}
+</style>
